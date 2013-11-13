829be2d9b5d4b8b9380eb5f467b8c6fe529c6ed9
Apprentice <apprentice@dbc-chicago031.local>
2013-11-13 14:31:31 -0600

    first commit

---
+++ app/controllers/index.rb
@@ -0,0 +1,12 @@
+get '/' do
+
+  @client ||= OAuth2::Client.new(
+    ENV['GOOGLE_CLIENT_ID'],
+    ENV['GOOGLE_CLIENT_SECRET'],
+    :site => 'https://accounts.google.com',
+    :token_url => '/o/oauth2/token',
+    :authorize_url => '/o/oauth2/auth'
+  )
+
+  erb :index
+end
